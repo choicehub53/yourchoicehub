d77a25a9fcdcf2fd7a3a09e96f4e3897464a73cb
choicehub53 <choicehub53@gmail.com>
2026-08-11 22:29:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 import os
 import resend
 
-app = Flask(**name**)
+app = Flask(__name__)
 
 resend.api_key = os.environ.get("RESEND_API_KEY")
 
