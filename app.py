7e0caf9512c4d029faf3782c3944f1b831c5bb9e
choicehub53 <choicehub53@gmail.com>
2026-08-12 10:11:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,31 +2,35 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-return render_template("home.html")
+    return render_template("home.html")
+
 
 @app.route("/about")
 def about():
-return render_template("about.html")
+    return render_template("about.html")
+
 
 @app.route("/appointment")
 def appointment():
-return render_template("appointment.html")
+    return render_template("appointment.html")
+
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-if request.method == "POST":
-return redirect("/thank-you")
+    if request.method == "POST":
+        return redirect("/thank-you")
 
-
-return render_template("contact.html")
+    return render_template("contact.html")
 
 
 @app.route("/card")
 def card():
-return render_template("card.html")
+    return render_template("card.html")
+
 
 @app.route("/thank-you")
 def thank_you():
-return render_template("thank-you.html")
+    return render_template("thank-you.html")
